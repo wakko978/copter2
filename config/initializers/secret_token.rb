de17828a7c9d783c8a0d6369bc1bffed2691db0f
Matt Larson <wakko978@gmail.com>
2013-05-21 17:07:51 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copter2::Application.config.secret_token = 'ef35efb31d966e35558d15572477888df8b886f51d9a8afa5e1a88b0f35ca5b6a0387c55196f5c8d4eb200c4bab4d78d5e58d7f9b02a5d130f46d47e4a220984'
